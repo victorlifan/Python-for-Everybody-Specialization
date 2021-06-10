723eba697624db494565979102ffa3b95399a61c
Fan <victorlifan@vip.qq.com>
2021-06-09 22:06:31 -0500

    week 3 and 4 py

---
+++ Using Databases with Python/1.sql
@@ -0,0 +1,4 @@
+SELECT 'XYZZY' || hex(User.name || Course.title || Member.role ) AS X FROM 
+    User JOIN Member JOIN Course 
+    ON User.id = Member.user_id AND Member.course_id = Course.id
+    ORDER BY X LIMIT 1;
